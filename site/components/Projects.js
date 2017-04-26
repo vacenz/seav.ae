9751953b1994ec87b7190ef93d335ad678da0a56
Steven Martin <stevenisekimartin@gmail.com>
2017-04-27 02:06:10 +1000

    Update styles

--- site/components/Projects.js
+++ site/components/Projects.js
@@ -14,47 +14,46 @@ export default class Projects extends Component {
 
   render() {
     return (
-      <div styleName='Projects'>
-
+      <div styleName='Wrapper'>
         <div styleName='TitleContainer'>
-          <h1 styleName='Title'>svsx</h1>
-          <p styleName='Description'>steven iseki martin</p>
+          <h1 styleName='Title'>steven iseki martin</h1>
         </div>
-
         <Social />
 
-        <Card
-          background='rgba(19, 73, 112, 0.95)'
-          title='dekucomponents.com'
-          image='http://i.imgur.com/2VgF4Ut.jpg'
-          link='http://deku.vace.nz/' />
+        <div styleName='Projects'>
+          <Card
+            background='rgba(19, 73, 112, 0.95)'
+            title='dekucomponents.com'
+            image='http://i.imgur.com/2VgF4Ut.jpg'
+            link='http://deku.vace.nz/' />
 
-        <Card
-          background='rgba(19, 73, 112, 0.95)'
-          title='cssmodul.es'
-          image='http://i.imgur.com/VIU31i9.jpg'
-          link='http://cssmodules.vace.nz/' />
+          <Card
+            background='rgba(19, 73, 112, 0.95)'
+            title='cssmodul.es'
+            image='http://i.imgur.com/KbxSEsl.png'
+            link='http://cssmodules.vace.nz/' />
 
-        <Card
-          background='rgba(19, 73, 112, 0.95)'
-          title='starjs'
-          image='http://i.imgur.com/FlBHLqs.png'
-          link='http://starjs.ooo/' />
+          <Card
+            background='rgba(19, 73, 112, 0.95)'
+            title='starjs'
+            image='http://i.imgur.com/FlBHLqs.png'
+            link='http://starjs.ooo/' />
 
-        <Card
-          title='vace.nz'
-          image='http://i.imgur.com/f4u22sg.png'
-          link='http://vace.nz' />
+          <Card
+            title='vace.nz'
+            image='http://i.imgur.com/f4u22sg.png'
+            link='http://vace.nz' />
 
-        <Card
-          title='trac.kz'
-          image='http://i.imgur.com/xUnPaBy.png'
-          link='http://trac.kz' />
+          <Card
+            title='trac.kz'
+            image='http://i.imgur.com/xUnPaBy.png'
+            link='http://trac.kz' />
 
-        <Card
-          title='snvx tumblr'
-          image='http://i.imgur.com/rAzeFu9.png'
-          link='http://steveniseki.tumblr.com/' />
+          <Card
+            title='snvx tumblr'
+            image='http://i.imgur.com/rAzeFu9.png'
+            link='http://steveniseki.tumblr.com/' />
+        </div>
       </div>
     )
   }
